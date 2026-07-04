8ba0285e45ceb31336f6c5ffeddd8cfb4289cade
nataliehoj <nhojnacke@byupathway.edu>
2026-07-04 12:45:29 -0700

    week 1 exercises

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("Hello Student!");
+
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is you last name? ");
+        string lname = Console.ReadLine();
+        
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What do you guess the number is? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Congrations you guessed it!");
+            }
+
+        }                    
     }
 }
